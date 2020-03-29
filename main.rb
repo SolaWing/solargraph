c7b6adb3464e82642c06254a3d342a42da62b5d2
solawing <316786359@qq.com>
2020-03-29 18:22:33 +0800

    fix: main.rb should use realpath to avoid path issue

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
-load File.expand_path '../bin/solargraph', __FILE__
+# frozen_string_literal: true
+
+# sympath will cause require error
+$LOAD_PATH.unshift File.realpath File.expand_path 'lib', __dir__
+load File.realpath File.expand_path 'bin/solargraph', __dir__
