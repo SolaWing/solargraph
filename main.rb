1dae42c26223b8c90ea464e286aaaff065db9e89
solawing <316786359@qq.com>
2020-10-13 15:58:19 +0800

    fix: ignore YAML nil pin

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+Dir.chdir(__dir__) do
+  require 'bundler/setup'
+  Bundler.require
+  Bundler.reset!
+end
 # sympath will cause require error
 $LOAD_PATH.unshift File.realpath File.expand_path 'lib', __dir__
 load File.realpath File.expand_path 'bin/solargraph', __dir__
