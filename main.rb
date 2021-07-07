68dcdfccf87c083b6d80e4be303846208cc4a5da
solawing <316786359@qq.com>
2021-07-07 20:26:44 +0800

    fix: global solargraph shouldn't use inherit bundler

--- main.rb
+++ main.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+if defined? Bundler
+  # pry invoke editor may inherit old bundle env. it's break solargraph(since not local)..
+  # local solargraph should use bin/solargraph directly
+  Bundler.unbundled_exec('ruby', __FILE__, *ARGV)
+end
 Dir.chdir(__dir__) do
   # ensure solargraph deps use the bundle version. but not limit other external gem's version
   require 'bundler/setup'
   Bundler.require
+  # NOTE: reset won't reset env，nor $LOAD_PATH
+  ENV.replace(Bundler.unbundled_env)
   Bundler.reset!
 end
 # sympath will cause require error
