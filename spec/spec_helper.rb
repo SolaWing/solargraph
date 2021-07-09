27d8e7d0736b5d5cb3a0fa2e3daccb008c931689
solawing <316786359@qq.com>
2021-07-10 00:47:59 +0800

    chore: spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,12 @@ SimpleCov.start
 require 'solargraph'
 # Suppress logger output in specs (if possible)
 Solargraph::Logging.logger.reopen(File::NULL) if Solargraph::Logging.logger.respond_to?(:reopen)
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
